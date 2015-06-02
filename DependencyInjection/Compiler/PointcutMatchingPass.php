bfcdb12e326d3fef70b52dfad979cd5b6f81a489
Adam Prager <prager.adam87@gmail.com>
2015-06-02 03:55:59 +0200

    Fixed deprecation notice

--- DependencyInjection/Compiler/PointcutMatchingPass.php
+++ DependencyInjection/Compiler/PointcutMatchingPass.php
@@ -99,7 +99,7 @@ class PointcutMatchingPass implements CompilerPassInterface
             return;
         }
 
-        if ($definition->getFactoryService() || $definition->getFactoryClass()) {
+        if ($definition->getFactoryService(false) || $definition->getFactoryClass(false)) {
             return;
         }
 
